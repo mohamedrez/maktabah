87701662fc9036fc77ceeae3854b4cc4fc0353ee
itsAndL <ayoub003lkebch@gmail.com>
2023-02-04 12:12:46 +0100

    Embed Youtube video in show view of the step model

--- app/controllers/steps_controller.rb
+++ app/controllers/steps_controller.rb
@@ -4,5 +4,12 @@ class StepsController < ApplicationController
   end
   def show
     @step = Step.find(params[:id])
+    @youtube_id = youtube_embed(Lecture.find(@step.stepable_id).youtube_video_link)
+  end
+
+  private
+  def youtube_embed(youtube_url)
+    youtube_url[/^.*((v\/)|(embed\/)|(watch\?))\??v?=?([^\&\?]*).*/]
+    youtube_id = $5
   end
 end
