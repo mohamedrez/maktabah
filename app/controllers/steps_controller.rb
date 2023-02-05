2919d07ad9a6c093e98fb702b56decc70dccf161
itsAndL <ayoub003lkebch@gmail.com>
2023-02-05 13:57:46 +0100

    modify something in step progress

--- app/controllers/steps_controller.rb
+++ app/controllers/steps_controller.rb
@@ -1,19 +1,23 @@
+require_relative '../../lib/helpers/lecture_helper'
+include LectureHelper
+
 class StepsController < ApplicationController
   def index
     @steps = Step.where(course_id: params[:course_id])
+    @steps_statuses = []
+    if current_user
+      @steps.each do |step|
+        user_progress = step.user_progresses.where!(user_id: current_user.id).first
+        @steps_statuses << {step: step, status: user_progress.status}
+      end
+    end
   end
   def show
     @step = Step.find(params[:id])
-    @youtube_id = youtube_embed(Lecture.find(@step.stepable_id).youtube_video_link)
+    @youtube_id = LectureHelper.youtube_embed(Lecture.find(@step.stepable_id).youtube_video_link)
     
     if current_user
       @step.user_progresses.first_or_create!(user_id: current_user.id).update!(status: 0)
     end
-  end
-
-  private
-  def youtube_embed(youtube_url)
-    youtube_url[/^.*((v\/)|(embed\/)|(watch\?))\??v?=?([^\&\?]*).*/]
-    youtube_id = $5
   end
 end
