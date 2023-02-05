3bdb228b938727394b83d699b15f0d8c1449e5e1
itsAndL <ayoub003lkebch@gmail.com>
2023-02-05 16:36:31 +0100

    some modification on steps_controller.rb file

--- app/controllers/steps_controller.rb
+++ app/controllers/steps_controller.rb
@@ -4,20 +4,29 @@ include LectureHelper
 class StepsController < ApplicationController
   def index
     @steps = Step.where(course_id: params[:course_id])
-    @steps_statuses = []
-    if current_user
-      @steps.each do |step|
-        user_progress = step.user_progresses.where!(user_id: current_user.id).first
-        @steps_statuses << {step: step, status: user_progress.status}
-      end
-    end
+    @steps_statuses = get_statuses(@steps)
   end
   def show
     @step = Step.find(params[:id])
     @youtube_id = LectureHelper.youtube_embed(Lecture.find(@step.stepable_id).youtube_video_link)
     
     if current_user
-      @step.user_progresses.first_or_create!(user_id: current_user.id).update!(status: 0)
+      @user_progress = @step.user_progresses.first_or_create!(user_id: current_user.id)
+      if(@user_progress.status != "completed")
+        @user_progress.update!(status: 0)
+      end
     end
+  end
+
+  private
+  def get_statuses(steps)
+    steps_statuses = []
+    if current_user
+      steps.each do |step|
+        user_progress = step.user_progresses.where!(user_id: current_user.id).first
+        steps_statuses << {step: step, status: user_progress.status}
+      end
+    end
+    steps_statuses
   end
 end
