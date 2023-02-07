75f9053408ee3f8dec0affeb6c0d317de4108564
Mohamed <mohamed.elidrissi@crossoverhealth.com>
2023-02-07 16:24:22 +0100

    user progress when step started

--- app/controllers/steps_controller.rb
+++ app/controllers/steps_controller.rb
@@ -4,5 +4,13 @@ class StepsController < ApplicationController
   end
   def show
     @step = Step.find(params[:id])
+    if current_user
+      @user_progress = UserProgress.first_or_create!(
+        user_id: current_user.id,
+        progressable_type: 'Step',
+        progressable_id: @step.id
+      )
+      @user_progress.update_attribute('status', :started) unless @user_progress.status
+    end
   end
 end
