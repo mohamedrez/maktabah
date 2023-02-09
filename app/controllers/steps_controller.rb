01d5cb0b5123f72406b3f7bf502651328005e164
itsAndL <ayoub003lkebch@gmail.com>
2023-02-09 09:08:13 +0100

    Add embedded youtube video and some small changes

--- app/controllers/steps_controller.rb
+++ app/controllers/steps_controller.rb
@@ -1,9 +1,13 @@
+require_relative '../../lib/helpers/lecture_helper'
+include LectureHelper
+
 class StepsController < ApplicationController
   def index
     @steps = Step.where(course_id: params[:course_id])
   end
   def show
     @step = Step.find(params[:id])
+    @youtube_id = youtube_embed(Lecture.find(@step.stepable_id).youtube_video_link)
     if current_user
       @user_progress = UserProgress.find_or_create_by!(user: current_user, progressable: @step)
 
