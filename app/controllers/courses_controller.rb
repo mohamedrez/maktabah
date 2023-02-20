317fd216104eaaeb768af9274f6e126b355ad6e0
itsAndL <ayoub003lkebch@gmail.com>
2023-02-20 11:46:31 +0100

    add form for Course

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -9,4 +9,23 @@ class CoursesController < ApplicationController
     @track = Track.find(params[:track_id])
     @courses = @track.courses
   end
+
+  def new 
+    @course = Course.new
+  end
+
+  def create 
+    @course = Course.new(course_params)
+    if @course.save
+      redirect_to @course, notice: "Great! The Course has been created!"
+    else
+      flash.now[:alert] = "Fix your mistakes, please."
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def course_params
+    params.require(:course).permit(:track_id, :position, :name, :description)
+  end
 end
