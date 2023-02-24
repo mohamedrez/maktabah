8aa519c6b3e34e4725d28ad3478072b01a482fad
itsAndL <ayoub003lkebch@gmail.com>
2023-02-24 16:44:58 +0100

    work on i18n

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,19 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  before_action :set_locale
+
   def after_sign_in_path_for(resource)
     stored_location_for(resource) || dashboard_index_path
+  end
+
+  private
+
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
+
+  def default_url_options(options = {})
+    {locale: I18n.locale}.merge options
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,23 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  mount Motor::Admin => "/motor_admin"
-  resources :home, only: [:index]
-  resources :dashboard, only: [:index]
-  devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  scope "(:locale)", locale: /en|ar/ do
+    mount Motor::Admin => "/motor_admin"
+    resources :home, only: [:index]
+    resources :dashboard, only: [:index]
+    devise_for :users
+    # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
-  resources :tracks, only: [:show, :index]
-  resources :profiles, only: [:show, :edit]
-  patch "/profiles/:id", to: "profiles#update", as: "update_profile"
-  resources :courses do
-    resources :steps, only: [:index, :show, :new, :create]
-    patch "/steps/:id", to: "steps#update_status", as: "update_step_status"
+    # Defines the root path route ("/")
+    # root "articles#index"
+    resources :tracks, only: [:show, :index]
+    resources :profiles, only: [:show, :edit]
+    patch "/profiles/:id", to: "profiles#update", as: "update_profile"
+    resources :courses do
+      resources :steps, only: [:index, :show, :new, :create]
+      patch "/steps/:id", to: "steps#update_status", as: "update_step_status"
+    end
+    post "/user_quiz_responses", to: "user_quiz_responses#create"
+    resources :user_progresses, only: [:create, :update]
   end
-  post "/user_quiz_responses", to: "user_quiz_responses#create"
-  resources :user_progresses, only: [:create, :update]
 end
