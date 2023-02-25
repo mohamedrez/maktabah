1b3c6fe98a66f81ff8ec6f9faa6759e8ddabe112
itsAndL <ayoub003lkebch@gmail.com>
2023-02-25 20:51:28 +0100

    work on home page

--- app/models/home.rb
+++ app/models/home.rb
@@ -10,18 +10,20 @@
 #
 class Home < ApplicationRecord
   def self.active_step(current_user)
-    UserProgress.where(user: current_user, progressable_type: "Step", status: :started).last!
+    up = UserProgress.where(user: current_user, progressable_type: "Step", status: :started).last!.progressable_id
+    Step.find(up)
   end
 
   def self.active_course(current_user)
-    UserProgress.where(user: current_user, progressable_type: "Course", status: :started).last!
+    up = UserProgress.where(user: current_user, progressable_type: "Course", status: :started).last!.progressable_id
+    Course.find(up)
   end
 
   def self.in_progress(current_user)
-    UserProgress.where(user: current_user, progressable_type: "Course", status: :started)
+    UserProgress.where(user: current_user, progressable_type: "Course", status: :started).pluck(:progressable_id)
   end
 
   def self.completed_courses(current_user)
-    UserProgress.where(user: current_user, progressable_type: "Course", status: :completed)
+    UserProgress.where(user: current_user, progressable_type: "Course", status: :completed).pluck(:progressable_id)
   end
 end
