3f1876f2870c76404a84c90d814afd18d0b897e6
Mohamed <mohamed.elidrissi@crossoverhealth.com>
2023-03-02 11:22:47 +0100

    force test

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,7 +73,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = {host: "localhost:3000"}
 
   # 5.2 Action Mailer Configuration for Gmail
-  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.delivery_method = :test
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
