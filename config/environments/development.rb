6d91d3ecc1168083fd4f26a72c75d6dcc602ba79
itsAndL <ayoub003lkebch@gmail.com>
2023-03-04 13:06:18 +0100

    fix a problem related to confirmation email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,7 +73,8 @@ Rails.application.configure do
   config.action_mailer.default_url_options = {host: "localhost:3000"}
 
   # 5.2 Action Mailer Configuration for Gmail
-  config.action_mailer.delivery_method = ENV.fetch("DELIVERY_METHOD", "smtp")
+  # config.action_mailer.delivery_method = ENV.fetch("DELIVERY_METHOD", "smtp")
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
