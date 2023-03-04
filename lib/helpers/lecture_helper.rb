7fa8c33a1ee0479519a8b34a6b91e5d7752eff86
itsAndL <ayoub003lkebch@gmail.com>
2023-03-04 08:55:24 +0100

    i maked a simple code to extract the id from youtube video

--- lib/helpers/lecture_helper.rb
+++ lib/helpers/lecture_helper.rb
@@ -2,17 +2,7 @@
 
 module LectureHelper
   def youtube_id(url)
-    youtube_formats = [
-      %r(https?://youtu\.be/(.+)),
-      %r(https?://www\.youtube\.com/watch\?v=(.*?)(&|#|$)),
-      %r(https?://www\.youtube\.com/embed/(.*?)(\?|$)),
-      %r(https?://www\.youtube\.com/v/(.*?)(#|\?|$)),
-      %r(https?://www\.youtube\.com/user/.*?#\w/\w/\w/\w/(.+)\b)
-    ]
-
-    url.strip!
-    return unless url.include? "youtu"
-
-    youtube_formats.find { |format| url =~ format } and Regexp.last_match(1)
+    v_index = url.index("v=")
+    url[v_index + 2..v_index + 12]
   end
 end
