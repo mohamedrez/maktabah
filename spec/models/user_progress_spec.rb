602f8e945c96af8df3fa337698795d9695c7db5d
itsAndL <ayoub003lkebch@gmail.com>
2023-03-04 10:32:41 +0100

    create a test for the Step#update_status method

--- spec/models/user_progress_spec.rb
+++ spec/models/user_progress_spec.rb
@@ -15,26 +15,25 @@
 require "rails_helper"
 
 RSpec.describe(UserProgress, type: :model) do
-  context "user completes all steps in a course" do
-    before do
-      @user = FactoryBot.create(:user)
-      @track = FactoryBot.create(:track)
-      @course = FactoryBot.create(:course, track: @track)
-      @lecture1 = FactoryBot.create(:lecture)
-      @lecture2 = FactoryBot.create(:lecture)
-      @step1 = FactoryBot.create(:step, course: @course, stepable: @lecture1)
-      @step2 = FactoryBot.create(:step, course: @course, stepable: @lecture2)
-    end
+  before do
+    @user = FactoryBot.create(:user, id: 1)
+    @user2 = FactoryBot.create(:user, id: 2)
+    @track = FactoryBot.create(:track)
+    @course = FactoryBot.create(:course, track: @track)
+    @lecture1 = FactoryBot.create(:lecture)
+    @lecture2 = FactoryBot.create(:lecture)
+    @step1 = FactoryBot.create(:step, course: @course, stepable: @lecture1)
+    @step2 = FactoryBot.create(:step, course: @course, stepable: @lecture2)
+  end
 
+  context "user completes all steps in a course" do
     it "creates completes a course progress when all the steps are completed " do
       user_progress1 = FactoryBot.create(
         :user_progress, user: @user, progressable: @step1, status: :started
-      )
-      user_progress1.update(status: :completed)
+      ).update(status: :completed)
       user_progress2 = FactoryBot.create(
         :user_progress, user: @user, progressable: @step1, status: :started
-      )
-      user_progress2.update(status: :completed)
+      ).update(status: :completed)
       expect(
         UserProgress.where(
           user_id: @user.id, progressable: @course
@@ -56,6 +55,20 @@ RSpec.describe(UserProgress, type: :model) do
           user_id: @user.id, progressable: @course
         ).first&.status
       ).to(eq(nil))
+    end
+  end
+
+  describe "#update_status" do
+    it "returns completed afer providing the step as started" do
+      up = FactoryBot.create(:user_progress, status: :started, progressable: @step1, user: @user2)
+      up.update_status
+      expect(up.status).to eql("completed")
+    end
+
+    it "returns started afer providing the step as completed" do
+      up = FactoryBot.create(:user_progress, status: :completed, progressable: @step1, user: @user2)
+      up.update_status
+      expect(up.status).to eql("started")
     end
   end
 end
