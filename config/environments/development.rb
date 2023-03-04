71964e8183bc5be271289164eed978e5200a6763
itsAndL <ayoub003lkebch@gmail.com>
2023-03-04 18:29:57 +0100

    add the MailHog

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -72,18 +72,11 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = {host: "localhost:3000"}
 
-  # 5.2 Action Mailer Configuration for Gmail
-  # config.action_mailer.delivery_method = ENV.fetch("DELIVERY_METHOD", "smtp")
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
-    port: 587,
-    domain: "localhost",
-    user_name: ENV.fetch("GMAIL_USERNAME", "none"),
-    password: ENV.fetch("GMAIL_PASSWORD", "none"),
-    authentication: "plain",
-    enable_starttls_auto: true,
-    open_timeout: 5,
-    read_timeout: 5
-  }
+  Rails.application.configure do
+    config.action_mailer.perform_deliveries = true
+    config.action_mailer.smtp_settings = {
+      address: "localhost",
+      port: 1025
+    }
+  end
 end
