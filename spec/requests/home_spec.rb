77bd170ab7c419fde2913bd0f69b027d5ec91b4a
itsAndL <ayoub003lkebch@gmail.com>
2023-03-04 11:47:26 +0100

    home_spec.rb

--- spec/requests/home_spec.rb
+++ spec/requests/home_spec.rb
@@ -4,7 +4,7 @@ require "rails_helper"
 
 RSpec.describe("Homes", type: :request) do
   describe "GET /index" do
-    xit "returns http success" do
+    it "returns http success" do
       get "/home"
       expect(response).to(have_http_status(:success))
     end
