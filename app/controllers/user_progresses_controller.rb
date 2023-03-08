4bd3f1b22029a85c6b6398326e7264dfdc563f64
itsAndL <ayoub003lkebch@gmail.com>
2023-03-08 15:22:39 +0100

    fix a problem related to mark the step as completed

--- app/controllers/user_progresses_controller.rb
+++ app/controllers/user_progresses_controller.rb
@@ -5,7 +5,7 @@ class UserProgressesController < ApplicationController
 
   def update
     step = Step.find(params[:step_id])
-    course = Course.find(step.id)
+    course = Course.find(params[:course_id])
 
     UserProgress.find_by(user: current_user, progressable: step).update_status
 
@@ -13,7 +13,7 @@ class UserProgressesController < ApplicationController
 
     respond_to do |format|
       format.html { redirect_to course_step_url(course, step) }
-      format.js { render "steps/update_status", locals: {step_id: step.id} }
+      format.js { render "steps/update_status", locals: {course_id: course.id, step_id: step.id} }
     end
   end
 end
