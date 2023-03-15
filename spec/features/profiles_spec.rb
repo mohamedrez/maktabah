b9a552f7b675bb30bd289563ffbda0b61f92dd94
itsAndL <ayoub003lkebch@gmail.com>
2023-03-15 14:24:42 +0100

    see if the bug gonna gone

--- spec/features/profiles_spec.rb
+++ spec/features/profiles_spec.rb
@@ -5,12 +5,12 @@ RSpec.describe 'visiting the edit profile page', type: :feature do
 
     visit new_user_session_url
     user = User.create! email: "email@eila.com", password: "somepassword", confirmation_token: "2023-03-15 13:24:03.915976000 +0000", confirmed_at: "2023-03-15 13:28:03.915976000 +0000"
-    fill_in "user_email", with: user.email
-    fill_in "user_password", with: user.password
+    fill_in "Email", with: user.email
+    fill_in "Password", with: user.password
     click_button "Log in"
 
     visit users_profile_edit_url
-    fill_in "user_username", with: "hi42"
+    fill_in "Username", with: "hi42"
     fill_in "user_learning_goal", with: "I wanna become a full stack RoR"
 
     click_button 'Save'
