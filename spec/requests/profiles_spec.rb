3bd0ed5ed88c17feae43690127c4d09707dcf77b
itsAndL <ayoub003lkebch@gmail.com>
2023-03-15 13:59:03 +0100

    using rspec for a request to users_profile_url

--- spec/requests/profiles_spec.rb
+++ spec/requests/profiles_spec.rb
@@ -1,7 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe "Profiles", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe "PATCH /update" do
+    it "changes the user's attributes" do
+      user = User.create! email: "email@eila.com", password: "somepassword", confirmation_token: "2023-03-15 13:24:03.915976000 +0000", confirmed_at: "2023-03-15 13:28:03.915976000 +0000"
+      sign_in user
+      patch users_profile_url, params: { user: { username: "hi42", learning_goal: "I wanna become a full stack RoR" } }
+      user.reload
+      expect(user.username).to eq('hi42')
+      expect(user.learning_goal).to eq('I wanna become a full stack RoR')
+    end
   end
 end
